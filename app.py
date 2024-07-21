48e64cdabf7d7fa171102f7425c183243f2c7e07
SanathMacha <115341038+SanathMacha@users.noreply.github.com>
2024-07-21 22:40:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,28 +5,27 @@ from main import callgem
 from gemini import gemin
 
 app=Flask(__name__)
-camera=cv2.VideoCapture(0)
 
-def generate_frames():
-    while True:
-        #read the camera frame
-        success,frame=camera.read()
-        if not success :
-            break
-        else:
-            ret,buffer=cv2.imencode('.jpg',frame)
-            frame = buffer.tobytes()
+# def generate_frames():
+#     while True:
+#         #read the camera frame
+#         success,frame=camera.read()s
+#         if not success :
+#             break
+#         else:
+#             ret,buffer=cv2.imencode('.jpg',frame)
+#             frame = buffer.tobytes()
 
-        yield(b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+#         yield(b'--frame\r\n'
+#               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/video')
-def video():
-    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+# @app.route('/video')
+# def video():
+#     return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/call')
 def call():
@@ -52,3 +51,5 @@ def codefunc(message):
 
 if __name__=='__main__':
     app.run(debug=True)
+
+
