eea4592176898868be09b558ecbf668f84e19fe5
SanathMacha <115341038+SanathMacha@users.noreply.github.com>
2024-07-21 22:41:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,48 @@ import speech_recognition as sr
 import time
 import requests
 import json
-import pyttsx3
+# import pyttsx3
 from news import speak_news
 
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+import pygame
+
+from gtts import gTTS
+
+
+import os 
+
+language = 'en'
+# engine = pyttsx3.init()
+# voices = engine.getProperty('voices')
+# engine.setProperty('voice', voices[0].id)
 # print("sdfghjkl")
+
+
+
 def speak(audio):
-    engine.stop()
-    engine.say(audio)
-    engine.runAndWait()
+    myobj = gTTS(text=audio, lang=language, slow=False)
+
+    myobj.save("welcome.mp3")
+
+    pygame.mixer.init()
+    try:
+        pygame.mixer.music.load("welcome.mp3")
+        pygame.mixer.music.play()
+        
+        # Wait for the sound to finish playing
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+        
+        print('Playing sound using pygame.mixer')
+    except Exception as e:
+        print(f"Error playing sound: {e}")
+    finally:
+        pygame.mixer.quit()  # Clean up pygame mixer
+
+
+
+    # engine.say(audio)
+    # engine.runAndWait()
 
 
 r=sr.Recognizer()
